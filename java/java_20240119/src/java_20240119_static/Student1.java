c2470ebd1a48b4d358879a5db64467f237b5f256
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2024-01-19 14:01:46 +0900

    Update Student1.java

--- java/java_20240119/src/java_20240119_static/Student1.java
+++ java/java_20240119/src/java_20240119_static/Student1.java
@@ -1,5 +1,43 @@
 package java_20240119_static;
 
 public class Student1 {
-
+int serialNum = 1000;
+	
+	int studentId;
+	String studentName;
+	int grade;
+	String address;
+	
+	public int getSerialNum() {
+		return serialNum;
+	}
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 }
