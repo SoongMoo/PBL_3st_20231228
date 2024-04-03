dedc165eef306f5da43aaf6510fb43f8baa8238b
HKEDU <HKEDU@DESKTOP-CMQ4SVM>
2024-04-03 16:26:06 +0900

    sms완료

--- springBootMVCShopping/src/main/java/springBootMVCShopping/service/SMSMassageService.java
+++ springBootMVCShopping/src/main/java/springBootMVCShopping/service/SMSMassageService.java
@@ -18,16 +18,13 @@ public class SMSMassageService {
 		String api_secret = "WN5E66CV7FMOEVUA52T0H0UQUZU0VQOY";
 		this.messageService = NurigoApp.INSTANCE.initialize(api_key, api_secret, "https://api.coolsms.co.kr");
 	}
-
 	public void smsSend(String _from,  String _to, String _content) {
 		Message message = new Message();
         // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
         message.setFrom(_from);
         message.setTo(_to);
         message.setText(_content);
-        SingleMessageSentResponse response = 
-        		this.messageService.sendOne(new SingleMessageSendingRequest(message));
-        System.out.println(response);
-				
+        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+        System.out.println(response);	
 	}
 }
