3e29726f52ca2ae166d1fb57ce6a1e96094c29eb
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2024-05-13 17:02:19 +0900

    Add files via upload

---
+++ python/gugu.py
@@ -0,0 +1,6 @@
+dan = int(input("단을 입력해주세요 : "))
+start = int(input("시작 숫자를 입력하세요 : "))
+end = int(input("마지막 숫자를 입력하세요 : "))
+while start <= end:
+    print(f"8 * {start} = {8 * start}")
+    start += 1
